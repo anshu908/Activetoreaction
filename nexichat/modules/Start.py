9edc911a8c1009d83b0c3801bbe36b50bcb9585d
TheAnshCoder <ytanshu20@gmail.com>
2025-01-18 16:15:13 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -2,33 +2,23 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from nexichat import nexichat
 
+OWNER = 6949414014
+
 @nexichat.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
     # Sending a video along with the reply text
     await message.reply_video(
         video="https://envs.sh/RCD.mp4",  # Replace with the actual video URL or local file path
         caption=(
-            f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
-━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-● ɪ ᴀᴍ  {(await client.get_me()).mention} !
-
-
-⦿━━━━━━━━━━━━━━━━━━━━━⦿
-❍ •  ɪ'ʟʟ ʀᴇᴀᴄᴛ ᴛᴏ ᴇᴠᴇʀʏ ᴍᴇssᴀɢᴇ  •
-│❍ • ʙᴇsᴛ ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ •
-│❍ • ɴᴏ ʟᴀɢs + ɴᴏ ᴀᴅs •
-│❍ • 24x7 ᴏɴʟɪɴᴇ sᴜᴘᴘᴏʀᴛ •
-│❍ • ᴀ ᴘᴏᴡᴇʀғᴜʟ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ •
-❍ • ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs •
-⦿━━━━━━━━━━━━━━━━━━━━━⦿
-
-❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone**"""
+            f"""**┌────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼──────•\n│◍ ʜᴇʏ {message.from_user.first_name} ,\n│◍ ɪ'ᴍ {(await client.get_me()).mention} !\n└──────────────────────•\n✦ ϻσsᴛ ᴘσᴡєꝛғυʟʟ ʀєᴧᴄᴛɪση ʙσᴛ  \n✦ ʙєsᴛ ᴄʟσηєʀ ʙσᴛ ση ᴛєʟєɢꝛᴧϻ \n✦ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone \n✦ ᴧᴅᴅ ϻє ɢꝛσυᴘ ᴛσ sєє ϻʏ ᴘσᴡєꝛ\n•──────────────────────•\n❖ 𝐏ᴏᴡᴇʀᴇᴅ ʙʏ ➟ [ʙσᴛ ᴄʜᴧϻʙєꝛ ](https://t.me/ll_BOTCHAMBER_ll)❤️‍🔥\n•──────────────────────•**"""
         ),
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("❖ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ❖", url="https://t.me/ReactionByBot?startgroup=true")],
-                [InlineKeyboardButton("• sᴜᴘᴘᴏꝛᴛ •", url="https://t.me/ur_rishu_143")]
+                [InlineKeyboardButton("• σᴡηєꝛ •", user_id=OWNER),
+                 InlineKeyboardButton("• ᴄʟσηє •", callback_data="CLONE")], 
+                [InlineKeyboardButton("• υᴘᴅᴧᴛє •", url="https://t.me/ll_BOTCHAMBER_ll"),
+                InlineKeyboardButton("• sυᴘᴘσꝛᴛ •", url="https://t.me/BOT_SUPPORT_GROUP7")]
             ]
         )
     )
