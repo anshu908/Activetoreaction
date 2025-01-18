32ac24e876c1e27e55a775fa142dfeb05b383d38
TheAnshCoder <ytanshu20@gmail.com>
2025-01-18 17:26:26 +0530

    Update Callback.py

--- nexichat/modules/Callback.py
+++ nexichat/modules/Callback.py
@@ -41,6 +41,20 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(HELP_BTN),
             disable_web_page_preview=True,
         )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 elif query.data == "CLONE":
         await query.message.edit(
             text=CLONEHELP_READ,
