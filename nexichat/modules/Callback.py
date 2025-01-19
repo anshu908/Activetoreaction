e7033a308094744d5d69ac2933a6213a656580c5
TheAnshCoder <ytanshu20@gmail.com>
2025-01-19 09:58:24 +0530

    Update Callback.py

--- nexichat/modules/Callback.py
+++ nexichat/modules/Callback.py
@@ -1,40 +1,31 @@
 import random
-from pymongo import MongoClient
 from pyrogram import Client, filters
-from pyrogram.errors import MessageEmpty
-from pyrogram.enums import ChatAction
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from nexichat.database.chats import add_served_chat
-from nexichat.database.users import add_served_user
-from config import MONGO_URL
-from nexichat import nexichat, mongo
-from pyrogram.enums import ChatMemberStatus as CMS
-from pyrogram.types import CallbackQuery
-import asyncio
-import config
-from nexichat import LOGGER, nexichat, db
-from nexichat.modules.helpers import (
-    ABOUT_BTN,
-    CLONEHELP_READ,
-    STARTWEL,
-    ABOUT_READ,
-    ADMIN_READ,
-    BACK,
-    CHATBOT_BACK,
-    CHATBOT_READ,
-    DEV_OP,
-    HELP_BTN,
-    HELP_READ,
-    MUSIC_BACK_BTN,
-    START,
-    TOOLS_DATA_READ,
-    
-)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from nexichat import nexichat
+
+# Dummy variables to avoid undefined errors
+HELP_READ = "Help text here"
+STARTWEL = "Welcome to the bot!"
+CLONEHELP_READ = "Clone help text here"
+START = "Back to start"
+ABOUT_READ = "About text here"
+ADMIN_READ = "Admin panel here"
+TOOLS_DATA_READ = "Tools data text here"
+CHATBOT_READ = "Chatbot commands"
+
+HELP_BTN = [[InlineKeyboardButton("Option 1", callback_data="OPTION1"), InlineKeyboardButton("Option 2", callback_data="OPTION2")]]
+ABOUT_BTN = [[InlineKeyboardButton("Back", callback_data="BACK")]]
+DEV_OP = [[InlineKeyboardButton("Developer", callback_data="DEV")]]
+MUSIC_BACK_BTN = [[InlineKeyboardButton("Back", callback_data="BACK")]]
+CHATBOT_BACK = [[InlineKeyboardButton("Back to Help", callback_data="BACK_HELP")]]
+CLONEBACK = [[InlineKeyboardButton("Back", callback_data="BACK")]]
+WELSTART_BOT = "Start Cloning"
 
 
 @nexichat.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     bot_id = client.me.id
+
     if query.data == "HELP":
         await query.message.edit_text(
             text=HELP_READ,
@@ -42,73 +33,61 @@ async def cb_handler(client: Client, query: CallbackQuery):
             disable_web_page_preview=True,
         )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-#Start back
-
     elif query.data == "STARTCLONE":
         await query.message.edit(
             text=STARTWEL,
-            reply_markup=InlineKeyboardButton(WELSTART_BOT),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(WELSTART_BOT, callback_data="START")]]),
         )
 
     elif query.data == "CLONE":
         await query.message.edit(
             text=CLONEHELP_READ,
-            reply_markup=InlineKeyboardButton(CLONEBACK),
-        )   
-#----------------------------------------------------------#   
+            reply_markup=InlineKeyboardMarkup(CLONEBACK),
+        )
+
     elif query.data == "CLOSE":
         await query.message.delete()
         await query.answer("Closed menu!", show_alert=True)
+
     elif query.data == "BACK":
         await query.message.edit(
             text=START,
             reply_markup=InlineKeyboardMarkup(DEV_OP),
         )
-    
+
     elif query.data == "ABOUT":
         await query.message.edit(
             text=ABOUT_READ,
             reply_markup=InlineKeyboardMarkup(ABOUT_BTN),
             disable_web_page_preview=True,
         )
+
     elif query.data == "ADMINS":
         await query.message.edit(
             text=ADMIN_READ,
             reply_markup=InlineKeyboardMarkup(MUSIC_BACK_BTN),
         )
+
     elif query.data == "TOOLS_DATA":
         await query.message.edit(
             text=TOOLS_DATA_READ,
             reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
         )
+
     elif query.data == "BACK_HELP":
         await query.message.edit(
             text=HELP_READ,
             reply_markup=InlineKeyboardMarkup(HELP_BTN),
         )
+
     elif query.data == "CHATBOT_CMD":
         await query.message.edit(
             text=CHATBOT_READ,
             reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
         )
+
     elif query.data == "CHATBOT_BACK":
         await query.message.edit(
             text=HELP_READ,
             reply_markup=InlineKeyboardMarkup(HELP_BTN),
-        )
-    
-    
+)
